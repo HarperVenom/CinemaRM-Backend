f2e77c3674e51f859bb503dfb7774edd5d02a480
HarperVenom <162710380+HarperVenom@users.noreply.github.com>
2024-06-08 12:28:54 +0300

    fix

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const universeRouter = require("./routes/universe.route");
 const userRouter = require("./routes/user.router");
 const corsOptions = require("./config/corsOptions");
 const connectDB = require("./config/dbConn");
+const { default: mongoose } = require("mongoose");
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
@@ -16,3 +18,17 @@ app.use(express.json());
 //routes
 app.use("/api/universes", universeRouter);
 app.use("/api/users", userRouter);
+
+mongoose.connection.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
